79c8d3b98bb55fce981ac5e0be618d53e3fb1386
Thắng Bùi <buiducthang1771996@gmail.com>
2025-01-25 16:41:37 +0700

    MOS

--- src/components/MediaDetail/ActorInfo.jsx
+++ src/components/MediaDetail/ActorInfo.jsx
@@ -1,10 +1,9 @@
 const ActorInfo = ({ actor }) => {
-  console.log(actor);
 
   return (
     <div className="overflow-hidden rounded-2xl border border-slate-300 bg-black/80 text-white shadow-sm">
       <img
-        src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2/fkIWDrXGH4x8mWEX2eK1kUL8u2n.jpg"
+        src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${actor?.profile_path}`}
         alt=""
       />
       <div className="p-3">
